d4a844d9ba5e5bee981056c7dd516f38e0e6a8c1
Adil Mohiuddin <amohi9046@gmail.com>
2019-11-11 13:45:28 -0800

    #8 - Eslint + Babel

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,13 @@
 module.exports = function (api) {
-    api.assertVersion('^7.7');
+  api.assertVersion('^7.7');
 
-    api.cache(false);
+  api.cache(false);
 
-    const plugins = [
-        ['@babel/plugin-proposal-nullish-coalescing-operator'],
-        ['@babel/plugin-proposal-optional-chaining'],
-    ];
+  const plugins = [
+    ['@babel/plugin-proposal-nullish-coalescing-operator'],
+    ['@babel/plugin-proposal-optional-chaining'],
+    ['@babel/plugin-transform-strict-mode']
+  ];
 
-    return { plugins };
+  return { plugins };
 };
