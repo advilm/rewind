7a483eefa84abbba618fffbef311d29ed6a0d60c
Adil Mohiuddin <amohi9046@gmail.com>
2019-11-12 23:24:20 -0800

    #13 - Message event update

--- src/events/message.js
+++ src/events/message.js
@@ -1,1 +1,5 @@
-exports.run = async (client, msg) => msg.author.bot || (msg.prefix = msg.content.match(/^(re+|rewind)\./i)?.[0]) && client.handler.run(msg);
+exports.run = async (client, msg) => {
+  msg.author.bot ||
+    (new RegExp(`^<@!?${client.user.id}>($|\\s*prefix)`).test(msg.content) ? msg.channel.send('My prefix is `re.`')
+      : (msg.prefix = msg.content.match(/^(re+|rewind)\./i)?.[0]) && client.handler.run(msg));
+};
