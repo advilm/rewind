cf7d44ef2a497d4130e4fd745209f5f1ba8bd3ae
Adil Mohiuddin <amohi9046@gmail.com>
2020-01-13 22:22:33 -0800

    #19 - Random change

--- src/events/message.js
+++ src/events/message.js
@@ -2,6 +2,6 @@ exports.run = async (client, msg) => {
 	if (msg.author.bot) return;
 
 	if (new RegExp(`^<@!?${client.user.id}>($|\\s*prefix)`).test(msg.content)) return msg.channel.send('My prefix is `re.`');
-  
-	if ((msg.prefix = msg.content.match(/^(re+|rewind)\./i)?.[0])) await client.handler.run(msg);
+
+	if ((msg.prefix = /^(?:re+|rewind)\./i.exec(msg.content)?.[0])) client.handler.run(msg);
 };
--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -3,7 +3,5 @@ exports.run = async (client, m, msg) => {
 
 	if (new RegExp(`^<@!?${client.user.id}>($|\\s*prefix)`).test(msg.content)) return msg.channel.send('My prefix is `re.`');
   
-	if ((msg.prefix = msg.content.match(/^(re+|rewind)\./i)?.[0])) {
-		client.handler.run(msg);
-	}
+	if ((msg.prefix = /^(?:re+|rewind)\./i.exec(msg.content)?.[0])) client.handler.run(msg);
 };
