5f583583ab6d7ad4fb042a5148e854b5dcaaffe3
Adil Mohiuddin <amohi9046@gmail.com>
2020-01-16 00:36:00 -0800

    #24 - Allowed any amount of spaces after prefix

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -3,5 +3,5 @@ exports.run = async (client, m, msg) => {
 
 	if (new RegExp(`^<@!?${client.user.id}>($|\\s*prefix)`).test(msg.content)) return msg.channel.send('My prefix is `re.`');
   
-	if ((msg.prefix = /^(?:re+|rewind)\./i.exec(msg.content)?.[0])) client.handler.run(msg);
+	if ((msg.prefix = /^(?:re+|rewind)\.\s*/i.exec(msg.content)?.[0])) client.handler.run(msg);
 };
