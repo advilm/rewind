8cdbbfca8e9fed34764221be8fcc4104ef059b00
root <root@ip-172-31-94-199.ec2.internal>
2020-02-24 18:35:24 +0000

    #47 - Changed walk to fs

--- src/commands/dev/restart.js
+++ src/commands/dev/restart.js
@@ -1,28 +1,24 @@
-const { walk } = require('walk');
-const { resolve } = require('path');
 const Command = require('../../structures/Command.js');
 
 class Restart extends Command {
 	constructor() {
 		super({
 			aliases: ['r'],
+			usage: '[Flags]?',
 			description: 'Restarts.',
 		});
 	}
 	async run(msg) {
-		const walker = walk('./src');
-    
-		walker.on('file', (root, stats, next) => {
-			delete require.cache[require.resolve(`${resolve(root)}/${stats.name}`)];
-			next();
-		});
+		if (msg.flags.a) Object.keys(require.cache).forEach(x => delete require.cache[x]);
+		else {
+			for (const module of this.client.utils.walk('src/commands')) delete require.cache[`${process.cwd()}/${module}`];
+			for (const module of this.client.utils.walk('src/events')) delete require.cache[`${process.cwd()}/${module}`];
+		}
 
 		delete this.client._events;
-    
-		walker.on('end', async () => {
-			this.client.load();
-			msg.reply('Reloaded `a bunch of stuff`.');
-		});
+
+		await this.client.load();
+		msg.reply('Reloaded `a bunch of stuff`.');
 	}
 }
 
