52cf17c74d4db67418077e5dd89f1df8cc1447b1
root <root@ip-172-31-94-199.ec2.internal>
2020-02-24 18:14:11 +0000

    #39 - Disabled prefix stuff temporarily

--- src/commands/misc/prefix.js
+++ src/commands/misc/prefix.js
@@ -8,13 +8,13 @@ class Prefix extends Command {
 		});
 	}
 
-	async run(msg) {
-		const guild = (await this.client.db.models.Guild.findOne({ where: { _id: msg.guild.id } }));
-		guild.prefix = this.client.utils.escapeRegEx(msg.content);
-		await guild.save();
-		await this.client.db.models.Guild.sync();
+	async run() {
+		// const guild = (await this.client.db.models.Guild.findOne({ where: { _id: msg.guild.id } }));
+		// guild.prefix = this.client.utils.escapeRegEx(msg.content);
+		// await guild.save();
+		// await this.client.db.models.Guild.sync();
 
-		msg.reply(`Prefix set to \`${msg.content}\``);
+		// msg.reply(`Prefix set to \`${msg.content}\``);
 	}
 }
 
--- src/events/message.js
+++ src/events/message.js
@@ -2,6 +2,10 @@ exports.run = async (client, msg) => {
 	if (msg.author.bot || !msg.guild) return;
 
 	if (new RegExp(`^<@!?${client.user.id}>\\s*(?:prefix)?$`).test(msg.content)) return msg.channel.send('My prefix is `re.`');
-	const prefix = (await client.db.models.Guild.findOne({ where: {_id: msg.guild.id}}))?.prefix;
-	if ((msg.prefix = new RegExp(`^(?:(?:re+|rewind)\\.|${prefix})\\s*`, 'i').exec(msg.content)?.[0])) client.handler.run(msg);
+
+	// const prefix = (await client.db.models.Guild.findOne({ where: {_id: msg.guild.id}}))?.prefix;
+	// if ((msg.prefix = new RegExp(`^(?:(?:re+|rewind)\\.|${prefix})\\s*`, 'i').exec(msg.content)?.[0])) client.handler.run(msg);
+
+	if ((msg.prefix = /^(?:re+|rewind)\.\s*/i.exec(msg.content)?.[0])) client.handler.run(msg);
+
 };
