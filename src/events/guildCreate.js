ed901b54aa5bd4035f7b52649fcd8c9e595c60ba
root <root@ip-172-31-94-199.ec2.internal>
2020-02-24 18:33:49 +0000

    #45 - Forgot to add .run

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,4 +1,4 @@
-module.exports = async (client, guild) => {
-	await client.db.models.Guild.create({ _id: guild.id});
+module.exports.run = async (client, guild) => {
+	await client.db.models.Guild.create({ _id: guild.id });
 	await client.db.models.Guild.sync();
 };
--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,4 +1,4 @@
-module.exports = async (client, guild) => {
+module.exports.run = async (client, guild) => {
 	(await client.db.models.Guild.findOne({ where: { _id: guild.id } })).destroy();
 	await client.db.models.Guild.sync();
 };
