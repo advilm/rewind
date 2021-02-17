c735f04a16266b2ae243ed64921bcbd11b63b93b
Adil Mohiuddin <amohi9046@gmail.com>
2021-02-17 03:27:32 +0000

    back

--- src/structures/client/Client.js
+++ src/structures/client/Client.js
@@ -1,4 +1,5 @@
 const { Manager } = require('../Manager.js');
+const { Sequelize } = require('sequelize');
 const { createConnection } = require('typeorm');
 const  { EntitySchema } = require('typeorm');
 const Redis = require('ioredis');
@@ -6,7 +7,7 @@ require('reflect-metadata');
 
 class Client extends require('discord.js').Client {
 	constructor() {
-		super({ disableMentions: 'everyone', fetchAllMembers: true });
+		super({ disableMentions: 'everyone', fetchAllMembers: true, intents: 641 });
 		this.sleep = require('util').promisify(setTimeout);
 
 		this.load();
@@ -19,7 +20,7 @@ class Client extends require('discord.js').Client {
 
 		this.once('ready', () => {
 			console.log(`Logged in as ${this.user.tag} with ${this.guilds.cache.size} guilds`);
-			
+
 			this.manager = new Manager(this, this.config.lavalink.nodes, {
 				user: this.user.id,
 				shards: 1,
@@ -36,32 +37,27 @@ class Client extends require('discord.js').Client {
 		this.messages = new Map();
 		this.utils = require('./Utils.js');
 		this.handler = new (require('./Handler.js'))(this).load();
-
-		// this.db = new (require('sequelize'))('postgres://advil:9046@localhost:5432/rewind', {logging: false});
-		// await this.db.authenticate();
-		// for (const model of fs.readdirSync('src/models')) this.db.define(model.slice(0, -3), require(`../../Models/${model}`));
-		// await this.db.sync();
 	}
 	
 	async loadConnections() {
-		//const connection = await createConnection({
-		//	type: 'postgres',
-		//	url: this.config.postgresql,
-		//	synchronize: true,
-		//	entities: [
-		//		new EntitySchema(require('../../entities/User.js')),
-		//		new EntitySchema(require('../../entities/Guild.js'))
-		//	]
-		//});
+		const connection = await createConnection({
+			type: 'postgres',
+			url: this.config.postgresql,
+			synchronize: true,
+			entities: [
+				new EntitySchema(require('../../entities/User.js')),
+				new EntitySchema(require('../../entities/Guild.js'))
+			]
+		});
 
-		//this.pg = {
-		//	user: connection.getRepository('User'),
-		//	guild: connection.getRepository('Guild')
-		//};
+		this.pg = {
+			user: connection.getRepository('User'),
+			guild: connection.getRepository('Guild')
+		};
 
 		//this.redis = new Redis(this.config.redis.ip, this.config.redis.db);
 
-		//this.bl = new Set((await this.pg.user.find({ where: { blacklisted: true }, select: [ 'id' ] })).map(x => x.id));
+		this.bl = new Set((await this.pg.user.find({ where: { blacklisted: true }, select: [ 'id' ] })).map(x => x.id));
 	}
 }
 
