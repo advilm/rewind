80a3e3bfdd6e0750d4bfb7eba4b89d66e5d49a2e
Adil Mohiuddin <amohi9046@gmail.com>
2021-03-08 19:48:35 -0800

    simple api for frontend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-new (require('./structures/Extender.js'))();
-new (require('./structures/Console.js'))();
-new (require('./structures/client/Client.js'))();
+new (require('./structures/extender.js'))();
+new (require('./structures/console.js'))();
+
+const req = require('@helperdiscord/centra');
+
+const client = new (require('./structures/Client.js'))();
 
 Reflect.defineProperty(Array.prototype, 'last', {
 	configurable: true,
@@ -23,3 +26,40 @@ Reflect.defineProperty(Map.prototype, 'push', {
 		return this;
 	}
 });
+
+const fastify = require('fastify')({ logger: true })
+
+fastify.register(require('fastify-cors'), {
+	origin: ['https://rewind.advil.cf', 'http://localhost:3000']
+})
+
+fastify.post('/authenticate', async (request, reply) => {
+	const res = await req('https://discord.com/api/v8/oauth2/token', 'POST')
+		.body(require('querystring').encode({
+			'client_id': client.user.id,
+			'client_secret': client.config.secret,
+			'grant_type': 'authorization_code',
+			'code': request.body.code,
+			'redirect_uri': 'https://rewind.advil.cf/callback',
+			'scope': 'identify guilds'
+		}))
+		.header({ 'Content-Type': 'application/x-www-form-urlencoded' })
+		.json()
+
+	const user = await req('https://discord.com/api/v8/users/@me', 'GET')
+		.header({ 'Authorization': `Bearer ${res.access_token}` })
+		.json()
+
+	console.log(user, res)
+
+	return { access_token: res.access_token, username: user.username }
+});
+
+(async () => {
+	try {
+		await fastify.listen(3001)
+	} catch (err) {
+		fastify.log.error(err)
+		process.exit(1)
+	}
+})();
