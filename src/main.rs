ca49cd8dcfa4759e80545658a2f68a4b805e1166
Adil Mohiuddin <amohi9046@gmail.com>
2021-04-22 14:35:29 -0700

    switching to rust

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+use futures::stream::StreamExt;
+use serde_derive::Deserialize;
+use std::{error::Error, fs};
+use twilight_cache_inmemory::{InMemoryCache, ResourceType};
+use twilight_gateway::{
+    cluster::{Cluster, ShardScheme},
+    Event,
+};
+use twilight_http::Client as HttpClient;
+use twilight_model::gateway::Intents;
+
+#[derive(Deserialize)]
+struct Config {
+    token: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let config: Config = toml::from_str(&fs::read_to_string("config.toml")?)?;
+
+    // This is the default scheme. It will automatically create as many
+    // shards as is suggested by Discord.
+    let scheme = ShardScheme::Auto;
+
+    // Use intents to only receive guild message events.
+    let cluster = Cluster::builder(&config.token, Intents::GUILD_MESSAGES)
+        .shard_scheme(scheme)
+        .build()
+        .await?;
+
+    // Start up the cluster.
+    let cluster_spawn = cluster.clone();
+
+    // Start all shards in the cluster in the background.
+    tokio::spawn(async move {
+        cluster_spawn.up().await;
+    });
+
+    // HTTP is separate from the gateway, so create a new client.
+    let http = HttpClient::new(&config.token);
+
+    // Since we only care about new messages, make the cache only
+    // cache new messages.
+    let cache = InMemoryCache::builder()
+        .resource_types(ResourceType::MESSAGE)
+        .build();
+
+    let mut events = cluster.events();
+
+    // Process each event as they come in.
+    while let Some((shard_id, event)) = events.next().await {
+        // Update the cache with the event.
+        cache.update(&event);
+
+        tokio::spawn(handle_event(shard_id, event, http.clone()));
+    }
+
+    Ok(())
+}
+
+async fn handle_event(
+    shard_id: u64,
+    event: Event,
+    http: HttpClient,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match event {
+        Event::MessageCreate(msg) => {
+            if msg.content == "!ping" {
+                http.create_message(msg.channel_id)
+                    .content("Pong!")?
+                    .await?;
+            }
+        }
+
+        Event::ShardConnected(_) => {
+            println!("Connected on shard {}", shard_id);
+        }
+        // Other events here...
+        _ => {}
+    }
+
+    Ok(())
+}
