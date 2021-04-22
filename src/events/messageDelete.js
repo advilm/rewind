a881cd67d943a1a67c761f6d25e27bef25c9cdb5
Adil Mohiuddin <amohi9046@gmail.com>
2021-04-22 14:36:06 -0700

    deleting js stuff

--- src/events/messageDelete.js
+++
@@ -1,1 +0,0 @@
-exports.run = async (client, msg) => client.messages.get(msg.id) && msg.channel.messages.cache.get(client.messages.get(msg.id))?.delete() && client.messages.delete(msg.id);
--- src/events/voiceStateUpdate.js
+++
@@ -1,7 +0,0 @@
-exports.run = async (client, oldState, newState) => {
-	if (newState.id === client.user.id && !newState.channel) {
-		client.players.get(newState.guild.id)?.channel?.send('Disconnected.');
-		client.manager.leave(newState.guild.id);
-		client.players.delete(newState.guild.id);
-	}
-};
